bb2012d9b1a5fa19fbf20b09f4d512288057cba8
git-sergBond <git-serg.bond@yandex.ru>
2018-04-16 19:47:09 +0300

    нарисовал формулы, привел к диагональной форме

---
+++ main.js
@@ -0,0 +1,109 @@
+var simplexApp = new Vue({
+    el: '#simplexApp',
+    data: {
+        highlights: {
+            //!!!!!!!!!!!!!! ДОБАВИТЬ КРУЖКИ ПЕРЕХОДА ПО ШАГАМ
+            //step        v-if  steps[].helight == true
+            cell: [],
+            row: [],
+            col: []
+        }
+    },
+    methods: {
+        highlighted_cell: function (step_id, row_id, col_id) {
+            //return step_id > 0 && this.highlights[step_id - 1].row === row_id && this.highlights[step_id - 1].col === col_id
+            return this.highlights.some((element)=>{
+                if(element.row === row_id && element.col === col_id && element.step === step_id) return true;
+            }) && true;
+        },
+        highlighted_row: function (step_id, row_id, col_id) {
+            return step_id > 0 && this.highlights[step_id - 1].row === row_id
+        },
+        highlighted_col: function (step_id, row_id, col_id) {
+            return step_id > 0 && this.highlights[step_id - 1].col === col_id
+        }
+    },
+    computed: {
+        //хранит шаги перехода от одной симплекс таблице к другой
+        steps: function() {
+            let steps = [];
+            const BAZIS = 0, H = 1, B = 6, X = 2;//столбцы матрицы
+            let first_matrix = [
+            //                     s1  s2 - в другой литре
+            //  базиc, H   x1  x2  x3  x4  B
+                [3  , 2 , -1,  1,  1,  0, 0],
+                [4  , 10,  5,  2,  0, -1, 0],
+                ['c' , 0,   1, -5,  0,  0]
+            ]
+            let tmp;//временная матрица для произведения вычислений
+            //
+            tmp = copyMatr(first_matrix);
+            tmp.name_action = 'скопировал начальные данные, для вывода исходных данных';
+            steps.push(tmp);
+            // 
+            tmp = copyMatr(first_matrix);
+            tmp.name_action = 'Выбираем в последней строке наибольшее (по модулю) отрицательное число ';
+            let last_row = tmp.length - 1;
+            let min = tmp[last_row][1];
+            min = tmp[last_row].reduce((min, current, index) => {
+                if (typeof current == 'number' && current < min) return current;
+                return min;
+            }, min);
+            let selected_col = tmp[last_row].indexOf(min);
+            /*
+            this.highlights.push({
+                row: last_row,
+                col: selected_col,
+                step: steps.length
+            })*/
+            steps.push(tmp);
+            //
+            tmp = copyMatr(tmp);
+            tmp.name_action = 'Вычислим b = Н / Элементы_выбранного_столбца';
+            for(let i = 0; i < tmp.length; i++)
+                if(typeof tmp[i][B] == 'number')
+                    tmp[i][B] = tmp[i][H]/tmp[i][selected_col];
+            steps.push(tmp);
+            //
+            tmp = copyMatr(tmp);
+            tmp.name_action = 'Среди вычисленных значений b выбираем наименьшее. \
+            Пересечение выбранных столбца и строки даст нам разрешающий элемент.';
+            min = tmp[0][B];
+            for(let i = 0; i < tmp.length; i++)
+                if(typeof tmp[i][B] == 'number')
+                    if(tmp[i][B] < min) min = tmp[i][B];
+            
+            tmp.name_action += min;
+            steps.push(tmp);
+            //
+            tmp = copyMatr(tmp);
+            tmp.name_action = 'Меняем базис на переменную соответствующую разрешающему элементу ';
+            let resolving_el = { row: 0, col: 0 }; //разрешающий элемент
+            for(let i = 0; i < tmp.length; i++)
+                if(tmp[i][B] == min) resolving_el.row = i;
+            resolving_el.col = selected_col - X; //преебразуем jстолбец (X1)  в данные удобные для прользователя
+            tmp[resolving_el.row][BAZIS] = resolving_el.col; 
+            steps.push(tmp);
+            //
+            tmp = copyMatr(tmp);
+            tmp.name_action = 'Теперь необходимо пересчитать все элементы симплекс-таблицы, кроме столбца b. \
+            Вот как это можно сделать: \
+            Сам разрешающий элемент обращается в 1. \
+            Для элементов разрешающей строки – aij(*) = aij / РЭ \
+            (то есть каждый элемент делим на значение разрешающего элемента и получаем новые данные). \
+            Для элементов разрешающего столбца – они просто обнуляются. \
+            Остальные элементы таблицы пересчитываем по правилу прямоугольника.';
+            tmp[resolving_el.row][resolving_el.col + 1] = 1;
+            steps.push(tmp);
+            return steps;
+        }
+    }
+});
+function copyMatr(matrix){
+//копирует матрицы
+    return matrix.map(e => {
+        if (e instanceof Array) 
+            return e.map(v => v);
+        return e;
+    });
+} 
